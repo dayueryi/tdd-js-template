8d0f60aa86cc2eed707b1b0e4dd8e5b70dbd33d1
wangjiawei <wangjiawei_nj@csii.com.cn>
2019-07-01 19:19:22 +0800

    add: chai

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,8 +1,9 @@
 import assert from 'assert';
 import { add } from '../src/example';
-
+import { expect } from 'chai'
 describe('example', function() {
   it('如果 a 等于 2 ，b 等于 3，当调用 add()，则结果等于 5', function() {
-    assert.equal(add(2, 3), 5);
+    // assert.equal(add(2, 3), 3);
+    expect(add(2,3)).to.equal(5);
   });
 });
